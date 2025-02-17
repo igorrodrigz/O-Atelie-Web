7ed4f38d317643a57052ab26783c15789325c9e6
Igor Rodrigues <137235250+igorrodrigz@users.noreply.github.com>
2025-02-17 00:12:55 -0300

    Update app.py
    Debug to False

--- app.py
+++ app.py
@@ -431,4 +431,4 @@ def delete_servico(id):
     return redirect(url_for('lista_servicos'))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
